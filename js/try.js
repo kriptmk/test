0fb21fb886d5ceb238aca34cdbb4a8ba8d2c7b38
Maks <kript.mk@gmail.com>
2020-07-14 00:39:36 +0300

    Первый коммит

---
+++ js/try.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let incr = 10,
+	decr = 10;
+incr++;
+++decr;
+
+console.log (incr);
+console.log (decr);
+
+
+
+
+
+// let age = prompt ("Wanna see apocalypsis?", `yes`);
+
+	// alert ("You came for whatching how will crash your Earth");
+
+// let name = true;
+//alert (typeof name);
+//name = String(name);
+//alert (typeof name);
+
+
+
+
+
+//let a = 1;
+//let b = 2;
+
+//let c = 3 - (a = b + 1);
+//alert (a); //3
+//alert (c); //0
+
+
+
+
+
+
+
+
+
+
